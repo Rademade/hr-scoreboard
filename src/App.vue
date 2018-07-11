c2e69e6d5c4af320f7e6351d299765da08e5fc8c
Maksym Shutiak <max@rademade.com>
2018-07-11 17:35:50 +0300

    Add frontend part

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <VacationsList />
+  </div>
+</template>
+
+<script>
+import VacationsList from './components/VacationsList.vue';
+
+export default {
+  name: 'app',
+  components: {
+    VacationsList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/VacationsList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="vacations">
+    <h1 class="vacations__title">Vacations</h1>
+    <ul >
+      <li v-for="item in data.objects">
+        {{item.name}}
+        {{item.vacancies.total}}
+        <ul>
+          <li v-for="vacancy in item.vacancies.objects">{{vacancy.position}}</li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import Vue from 'vue';
+import VueResources from 'vue-resource';
+
+Vue.use(VueResources);
+
+export default {
+  data() {
+    return {
+      data: []
+    }
+  },
+  created() {
+    this.$http.post('/api/vacations').then(response => {
+      this.data = response.body;
+    }, response => {
+      return response;
+    });
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
+
+
+</style>
