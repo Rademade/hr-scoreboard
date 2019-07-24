ff3fd1e465a341b20bf9f83f5e912b9821695f66
Hegefix <hedgefix@gmail.com>
2019-07-24 17:31:18 +0300

    add auth

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const favicon = require("express-favicon");
 const path = require("path");
+const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
 const port = process.env.PORT || 8080;
 const app = express();
+
 app.use(favicon(__dirname + "/build/favicon.ico"));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
-app.get('/ping', function (req, res) {
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(pino);
+
+app.get('/ping', (req, res) => {
  return res.send('pong');
 });
-app.get('/*', function (req, res) {
+
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, "build', 'index.html"));
 });
+
+app.get("/hr/person/auth", (req, res) => {
+  console.log("aaaa", req, res);
+});
+
 app.listen(port);
