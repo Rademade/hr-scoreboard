6a13de5740fc89b3616615dfed71ac7749c42a1e
Hegefix <hedgefix@gmail.com>
2019-08-07 18:01:24 +0300

    changed laoyout

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -24,7 +24,7 @@ const Scoreboard = () => (
       }
 
       return items.map((item, index) => (
-        <Vacancy {...item} index={index} key={item.position} />
+        <Vacancy {...item} key={item.position} />
       ));
     }}
   </Wrapper>
