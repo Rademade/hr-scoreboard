1adfe763532948caf7c0802d1d9becbe32a4fcb3
Hegefix <hedgefix@gmail.com>
2019-09-06 11:04:56 +0300

    auth in cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
 const functions = require("firebase-functions");
+const axios = require("axios");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+const URL = "https://cleverstaff.net";
 
 exports.scheduledFunction = functions.pubsub
   .schedule("every 2 minutes")
   .onRun(async context => {
-    console.log("Run every 2 minutes", context);
-    return null;
+    console.log("Run every 2 minutes", context, URL);
+    try {
+      const authResp = await axios.post(URL + "/hr/person/auth", {
+        login: functions.config().auth.username,
+        password: functions.config().auth.password
+      });
+    } catch (error) {
+      console.log("Error:", error);
+    } finally {
+      return null;
+    }
   });
