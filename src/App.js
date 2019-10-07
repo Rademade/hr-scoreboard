27ed0f0c3862795d5ecdeef88204537f1dc3558b
Hegefix <hedgefix@gmail.com>
2019-10-07 19:03:15 +0300

    remove server packages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useReducer, createContext, useEffect } from "react"
+import React, { useReducer, createContext } from "react"
 import firebase from "firebase"
-import axios from "axios"
+// import axios from "axios"
 import { appReducer, initialState } from "./reducer"
 import Scoreboard from "./components/Scoreboard"
 import { firebaseConfig } from "./config"
@@ -9,22 +9,21 @@ firebase.initializeApp(firebaseConfig)
 export const AppStateContext = createContext({})
 
 const App = () => {
-  const [state, dispatch] = useReducer(appReducer, initialState)
+  const [state] = useReducer(appReducer, initialState)
   console.log("app state", state)
 
-  useEffect(() => {
-    async function doAuthPing() {
-      try {
-        console.log("test", document.cookie)
-        const response = await axios.get("/authPing", { withCredentials: true })
-        console.log("response", response.data)
-      } catch(error) {
-        console.log(error)
-        dispatch({ type: "SET_ERROR", payload: error.message })
-      }
-    }
-    doAuthPing()
-  }, [])
+  // useEffect(() => {
+  //   async function pingAuth() {
+  //     try {
+  //       const response = await axios.get("/authPing", { withCredentials: true })
+  //       console.log("response", response.data)
+  //     } catch(error) {
+  //       console.log(error)
+  //       dispatch({ type: "SET_ERROR", payload: error.message })
+  //     }
+  //   }
+  //   pingAuth()
+  // }, [])
 
   // useEffect(() => {
   //   async function doAuth() {
